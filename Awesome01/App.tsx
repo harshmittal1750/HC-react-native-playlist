fb590f8d9b278673e5868d25d4a73c75f5f61440
HARSH MITTAL <harshmittal1750@gmail.com>
2023-02-11 19:36:36 +0530

    till final setup

---
+++ Awesome01/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {View, Text, SafeAreaView} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World !</Text>
+        <Text>Hello World !</Text>
+        <Text>Hello World !</Text>
+        <Text>Hello World !</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ Awesome01/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import AppPro from './AppPro';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => AppPro);
