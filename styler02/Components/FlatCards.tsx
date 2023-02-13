b6f49ca0e1e64f17d0239355fd6047536c639f65
HARSH MITTAL <harshmittal1750@gmail.com>
2023-02-13 16:24:49 +0530

    all about flexbox

--- styler02/Components/FlatCards.tsx
+++ styler02/Components/FlatCards.tsx
@@ -4,9 +4,49 @@ import React from 'react';
 export default function FlatCards() {
   return (
     <View>
-      <Text>FlatCards</Text>
+      <Text style={styles.headingText}>FlatCards</Text>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text>red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text>green</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text>blue</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 8,
+  },
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 4,
+    margin: 8,
+  },
+  cardOne: {
+    backgroundColor: '#EF5354',
+  },
+  cardTwo: {
+    backgroundColor: '#50DBB4',
+  },
+  cardThree: {
+    backgroundColor: '#5DA3FA',
+  },
+});
