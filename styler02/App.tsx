5819ee11dab584e88bc88a6d02b56165548e7929
HARSH MITTAL <harshmittal1750@gmail.com>
2023-03-13 07:07:50 +0530

    app 3

--- styler02/App.tsx
+++ styler02/App.tsx
@@ -1,4 +1,4 @@
-import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import {View, SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
 import FlatCards from './Components/FlatCards';
 import ElevatedCards from './Components/ElevatedCards';
@@ -11,7 +11,7 @@ const App = () => {
     <SafeAreaView>
       <ScrollView>
         <View>
-          <Text>App</Text>
+          {/* <Text>App</Text> */}
 
           <FlatCards />
           <ElevatedCards />
